762975a0eafd194d9ae135b3406bb9947c0c4f21
Keren Gold <kerengold55@gmail.com>
2020-05-30 19:46:43 +0300

    Add files via upload

--- sed.py
+++ sed.py
@@ -8,12 +8,16 @@ import sys
 import os
 def sed(firstStr, secondStr, file):
     if not os.path.exists(file):
-        print("Provided file",file,"not exist")
+        print("Provided file",file," does not exist.")
+        print("Usage1: python",sys.argv[0],"-f","\"pattern\"","\"replacement\"","file")
+        print("Usage2: python",sys.argv[0],"\"pattern\"","\"replacement\"","\"string\"")
         return 1
     with open(file, "r") as sources:
         lines = sources.readlines()
+        #print(lines)
     
     reg=r""+ firstStr
+    #print(reg)
     with open(file, "w") as sources:
         for line in lines:
             # print(re.sub(reg, secondStr, line))
@@ -21,16 +25,28 @@ def sed(firstStr, secondStr, file):
 
 
 
-
 n = len(sys.argv)
-print(sys.argv)
-if n!=4:
-	print("You have provided invalid number of arguments (Expecting 3)")
-else:
+
+#works on file
+if n==4:
     pattrn=sys.argv[1]
     replacement=sys.argv[2]
-    file=sys.argv[3]
+    string=sys.argv[3]
+    reg=r""+ pattrn
+    print(re.sub(reg, replacement, string))
+
+#works on string    
+elif n==5 and sys.argv[1]=="-f":
+    pattrn=sys.argv[2]
+    replacement=sys.argv[3]
+    file=sys.argv[4]
     sed(pattrn,replacement,file)
+    
+else:
+    print("You have provided invalid number of arguments")
+    print("Usage1: python",sys.argv[0],"-f","\"pattern\"","\"replacement\"","file")
+    print("Usage2: python",sys.argv[0],"\"pattern\"","\"replacement\"","\"string\"")
+    
 
 
 
